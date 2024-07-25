bd0dc9b665f73abf8454a48a0fa0b32bc2b4b46b
suizhihao <suizhihao@pjlab.org.cn>
2024-07-25 19:12:12 +0800

    update

--- scripts/sh/save_eval_result_cifar10_ft_ailab.sh
+++ scripts/sh/save_eval_result_cifar10_ft_ailab.sh
@@ -13,24 +13,24 @@ if [ ! -d "$SAVE_DATA_BASE_DIR" ]; then
     exit 1
 fi
 
- nohup python -u main_forget.py --save_dir ./outputs/resnet18_cifar10/retrain --unlearn retrain --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch resnet18 --resume --shuffle --eval_result_ft
+nohup python -u main_forget.py --save_dir $OUTPUT_BASE_DIR/resnet18_cifar10/retrain --unlearn retrain --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch resnet18 --resume --shuffle --eval_result_ft
 
- nohup python -u main_forget.py --save_dir ./outputs/resnet18_cifar10/FT --unlearn FT --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch resnet18 --resume --shuffle --eval_result_ft
+nohup python -u main_forget.py --save_dir $OUTPUT_BASE_DIR/resnet18_cifar10/FT --unlearn FT --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch resnet18 --resume --shuffle --eval_result_ft
 
- nohup python -u main_forget.py --save_dir ./outputs/resnet18_cifar10/GA --unlearn GA --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch resnet18 --resume --shuffle --eval_result_ft
+nohup python -u main_forget.py --save_dir $OUTPUT_BASE_DIR/resnet18_cifar10/GA --unlearn GA --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch resnet18 --resume --shuffle --eval_result_ft
 
- nohup python -u main_forget.py --save_dir ./outputs/resnet18_cifar10/FF --unlearn fisher --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch resnet18 --resume --shuffle --eval_result_ft
+nohup python -u main_forget.py --save_dir $OUTPUT_BASE_DIR/resnet18_cifar10/FF --unlearn fisher --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch resnet18 --resume --shuffle --eval_result_ft
 
- nohup python -u main_forget.py --save_dir ./outputs/resnet18_cifar10/IU --unlearn wfisher --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch resnet18 --resume --shuffle --eval_result_ft
+nohup python -u main_forget.py --save_dir $OUTPUT_BASE_DIR/resnet18_cifar10/IU --unlearn wfisher --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch resnet18 --resume --shuffle --eval_result_ft
 
- nohup python -u main_forget.py --save_dir ./outputs/resnet18_cifar10/FT_prune --unlearn FT_prune --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch resnet18 --resume --shuffle --eval_result_ft
+nohup python -u main_forget.py --save_dir $OUTPUT_BASE_DIR/resnet18_cifar10/FT_prune --unlearn FT_prune --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch resnet18 --resume --shuffle --eval_result_ft
 
- nohup python -u main_forget.py --save_dir ./outputs/vgg16_cifar10/retrain --unlearn retrain --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch vgg16_bn_lth  --resume --shuffle --eval_result_ft
+nohup python -u main_forget.py --save_dir $OUTPUT_BASE_DIR/vgg16_cifar10/retrain --unlearn retrain --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch vgg16_bn_lth  --resume --shuffle --eval_result_ft
 
- nohup python -u main_forget.py --save_dir ./outputs/vgg16_cifar10/FT --unlearn FT --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch vgg16_bn_lth  --resume --shuffle --eval_result_ft
+nohup python -u main_forget.py --save_dir $OUTPUT_BASE_DIR/vgg16_cifar10/FT --unlearn FT --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch vgg16_bn_lth  --resume --shuffle --eval_result_ft
 
- nohup python -u main_forget.py --save_dir ./outputs/vgg16_cifar10/GA --unlearn GA --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch vgg16_bn_lth  --resume --shuffle --eval_result_ft
+nohup python -u main_forget.py --save_dir $OUTPUT_BASE_DIR/vgg16_cifar10/GA --unlearn GA --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch vgg16_bn_lth  --resume --shuffle --eval_result_ft
 
- nohup python -u main_forget.py --save_dir ./outputs/vgg16_cifar10/IU --unlearn wfisher --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch vgg16_bn_lth  --resume --shuffle --eval_result_ft
+nohup python -u main_forget.py --save_dir $OUTPUT_BASE_DIR/vgg16_cifar10/IU --unlearn wfisher --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch vgg16_bn_lth  --resume --shuffle --eval_result_ft
 
- nohup python -u main_forget.py --save_dir ./outputs/vgg16_cifar10/FT_prune --unlearn FT_prune --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch vgg16_bn_lth  --resume --shuffle --eval_result_ft
+nohup python -u main_forget.py --save_dir $OUTPUT_BASE_DIR/vgg16_cifar10/FT_prune --unlearn FT_prune --class_to_replace 0,1,3,5,6 --num_indexes_to_replace 2250 --arch vgg16_bn_lth  --resume --shuffle --eval_result_ft
